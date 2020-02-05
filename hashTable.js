cb1385ed40466a6b64f50f3a4a5a8d212ab93f94
Jacob Cacciamani <cacciamani10@gmail.com>
2020-02-05 17:16:38 -0500

    Implemented hash table for counting words

---
+++ hashTable.js
@@ -0,0 +1,55 @@
+const hashString = (word, max) => {
+    const c = 34;
+    let hash = 0;
+    for (letter of word) {
+        hash = c * hash + letter.charCodeAt(0);
+    }
+    return hash % max + 1;
+}
+
+exports.hashTable = (lyricsArr, maxBucket) => {
+    let table = [];
+    let collisionCount = 0;
+    for (word of lyricsArr) {
+        let hash = hashString(word, 1000);
+        // Location Empty (new word)
+        if (typeof table[hash] === 'undefined') {
+            console.log('New word', word, 'added at', hash);
+            table[hash] = [{
+                word: word,
+                count: 1
+            }]
+        }
+        // Location occupied
+        else  {
+            let found = false;
+            
+            // Iterate through buckets
+            for (wordObj of table[hash]) {
+                if (wordObj.word === word) {
+                    console.log('Word', word, 'found second time at', hash);
+                    wordObj.count++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                console.log('Collison at', hash);
+                collisionCount++;
+                if (table[hash].length <= maxBucket) {
+                    console.log('Pushed word', word, 'at', hash);
+                    table[hash].push({
+                        word: word,
+                        count: 1
+                    })
+                }
+                else {
+                    throw `Bucket limit exceeded at ${hash}! ${word} not added`;
+                }
+            }
+        }
+    }
+    console.log('Final size of array:', table.length);
+    console.log('Final collision count:', collisionCount);
+}
+
